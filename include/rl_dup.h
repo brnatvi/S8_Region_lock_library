3f3852d4ea684077eb6254339f9bceb21b5e468c
tharsiya <tharsiyavigneswaran@gmail.com>
2023-04-06 15:15:12 +0200

    update rl_dup

--- include/rl_dup.h
+++ include/rl_dup.h
@@ -3,4 +3,4 @@
 rl_descriptor rl_dup(rl_descriptor lfd);
 rl_descriptor rl_dup2(rl_descriptor lfd, int newd);
 rl_descriptor dup_function(rl_descriptor lfd, int newd);
-int add_new_owner(owner lfd_owner, owner new_owner);
+int add_new_owner(rl_descriptor lfd, int newd);
--- src/rl_dup.c
+++ src/rl_dup.c
@@ -5,8 +5,7 @@ rl_descriptor rl_dup(rl_descriptor lfd){
 
     // Gestion erreur
     if(newd == -1) {
-        PROC_ERROR("dup() failure");
-        // no close of newd (ref man dup)
+        PROC_ERROR("dup() failure"); // no close of newd (ref man dup)
         rl_descriptor bad = {.d = -1, .f = NULL};
         return bad;
     }
@@ -23,45 +22,40 @@ rl_descriptor rl_dup2(rl_descriptor lfd, int newd) {
 }
 
 rl_descriptor dup_function(rl_descriptor lfd, int newd) {
-    owner lfd_owner = {.proc = getpid(), .des = lfd.d};
-    owner new_owner = {.proc = getpid(), .des = newd};
-
-    if(add_new_owner(lfd_owner, new_owner) == -1){
+    if(add_new_owner(lfd, newd) == -1){
         PROC_ERROR("rl_dup2() failure : NB_LOCKS at max");
         rl_descriptor bad = {.d = -1, .f = NULL};
         CLOSE_FILE(newd);
         return bad;
     }
-
     rl_descriptor new_rl_descriptor = {.d = newd, .f = lfd.f};
     return new_rl_descriptor;
 }
 
-int add_new_owner(owner lfd_owner, owner new_owner) {
-    for(int i = 0; i < rl_all_files.nb_files; i++){
-        // index of first rl_lock in lock_table
-        int ind = rl_all_files.tab_open_files[i]->first;
-        /* parcourir lock_table jusqu'au dernier élément indiqué par -1 */
-        while(ind != -1){
-            size_t nbOwners = rl_all_files.tab_open_files[i]->lock_table[ind].nb_owners;
+int add_new_owner(rl_descriptor lfd, int newd) {
+    owner lfd_owner = {.proc = getpid(), .des = lfd.d};
+    owner new_owner = {.proc = getpid(), .des = newd};
 
-            // parcours de lock_owners
-            for(size_t j = 0; j < nbOwners; j++){
-                if(ownerEquals(rl_all_files.tab_open_files[i]->lock_table[ind].lock_owners[j], lfd_owner)) {
-                    // test si place pour ajouter nouveau propriétaire
-                    if(nbOwners < NB_OWNERS) {
-                        rl_all_files.tab_open_files[i]->lock_table[ind].lock_owners[nbOwners] = new_owner;
-                        rl_all_files.tab_open_files[i]->lock_table[ind].nb_owners++;
-                        break;
-                    }
-                    else {
-                        return -1;
-                    }
+    int ind = lfd.f->first;
+    while(ind != -1) {
+        size_t nbOwners = lfd.f->lock_table[ind].nb_owners;
+
+        // parcours de lock_owners
+        for(size_t j = 0; j < nbOwners; j++){
+            if(ownerEquals(lfd.f->lock_table[ind].lock_owners[j], lfd_owner)) {
+                // test si place pour ajouter nouveau propriétaire
+                if(nbOwners < NB_OWNERS) {
+                    lfd.f->lock_table[ind].lock_owners[nbOwners] = new_owner;
+                    lfd.f->lock_table[ind].nb_owners++;
+                    break;
+                }
+                else {
+                    return -1;
                 }
             }
-            // indice du prochain rl_lock
-            ind = rl_all_files.tab_open_files[i]->lock_table[ind].next_lock;
         }
+        // indice du prochain rl_lock
+        ind = lfd.f->lock_table[ind].next_lock;       
     }
     return 0;
 }
